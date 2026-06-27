067374753720017df590e91a13de8cd5b22adf2c
Christian Adi <c14220002@john.petra.ac.id>
2026-06-27 19:22:15 +0700

    revisi sidang (tambah logic login dan riwayat order)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,6 @@
 import { supabase } from '@/lib/supabase'
 import Navigation from '@/components/Navigation'
-import ProductCard from '@/components/ProductCard'
-import { Product } from '@/types/product'
+import ProductGrid from '@/components/ProductGrid'
 
 // FORCE SSG
 export const dynamic = 'force-static'
@@ -53,11 +52,7 @@ export default async function HomePage() {
                 </div>
 
                 {/* Products Grid */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                    {products?.map((product: Product) => (
-                        <ProductCard key={product.id} product={product} />
-                    ))}
-                </div>
+                <ProductGrid products={products ?? []} />
             </main>
         </>
     )
